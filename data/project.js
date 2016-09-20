542511aaeec3df0656888fe7d33a00fd259303ef
qyer0222 <474142609@qq.com>
2016-09-20 21:45:24 +0800

    project

--- data/project.js
+++ data/project.js
@@ -15,7 +15,7 @@
 var projects = [
 		{
 			projectName:"仿QQ浏览器",
-			projectWebsite:"http://2013.miaov.com/miaov_demo/diagnosite/",
+			projectWebsite:"https://qyer0222.github.io/QQ浏览器/",
 		    startTime:"2016.04.01",
 			endTime:"至今",
 			projectExplain:"JS整站开发之：整屏切换+CSS3、导航条和首屏出场动画、3D翻转实现、整屏切换。",
@@ -24,7 +24,7 @@ var projects = [
 		},
 		{
 			projectName:"firmorama",
-			projectWebsite:"http://v.miaov.com/thanks/",
+			projectWebsite:"https://qyer0222.github.io/firmorama/",
 		    startTime:"2016.01.01",
 			endTime:"至今",
 			projectExplain:"用户观看页面的图片，其大部分内容是通过JS加载的。为了提高性能，采用了延迟加载、延迟渲染、事件代理等优化方式。此外，还通过监听页面的window.onchang事件对图片进行动态。",
@@ -33,7 +33,7 @@ var projects = [
 		},
 		{
 			projectName:"CV",
-			projectWebsite:"http://v.miaov.com/thanks/",
+			projectWebsite:"https://qyer0222.github.io/CV/",
 		    startTime:"2016.01.01",
 			endTime:"至今",
 			projectExplain:"用户观看页面的图片，其大部分内容是通过JS加载的。为了提高性能，采用了延迟加载、延迟渲染、事件代理等优化方式。此外，还通过监听页面的window.onchang事件对图片进行动态。",
@@ -42,7 +42,7 @@ var projects = [
 		},
 		{
 			projectName:"签到送礼",
-			projectWebsite:"http://v.miaov.com/thanks/",
+			projectWebsite:"https://qyer0222.github.io/签到/",
 		    startTime:"2016.05.01",
 			endTime:"至今",
 			projectExplain:"采用日历对象、原生js编写，每次打开页面都会同步当前日期，实时同步当前时间（北京），点击签到显示当前今天已经签到",
