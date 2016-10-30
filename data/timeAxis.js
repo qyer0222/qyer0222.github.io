d9355db953b6bb32d62e43721bbc3c1ed7c1fabe
qyer0222 <474142609@qq.com>
2016-10-30 10:36:08 +0800

    project

--- data/timeAxis.js
+++ data/timeAxis.js
@@ -16,23 +16,13 @@
 
 var timeAxisArr = [
 		{
-		    startTime:"2013/08",
-			endTime:"2014/06",
-			companyLogo:'images/icon/expre_logo1.png',
-			companyNam:"亚信联创",
-			companyWebsite:"http://www.asiainfo.com.cn/index.html;jsessionid=62355E2A4EE79A92B09256B3A58F7489",
-			jobPost:"手机端测试",
-			jonTask:"QA",
-			jobContent:"包括：1、负责Android、IOS客户端产品系统性测试（功能、交互、适配兼容、基础性能、易用性 2、参与项目组产品需求讨论分析，整体负责每个迭代测试需求分析及用例编写； 3、以实际用户角度体验产品设计，提交改进意见； 4、有效准确定位bug，并推动bug修复；"
-		},
-		{
 		    startTime:"2014/08",
 			endTime:"2016/09",
-			companyLogo:'images/icon/expre_logo2.png',
-			companyNam:"明顺科技有限公司",
-			companyWebsite:"http://www.miaov.com",
-			jobPost:"前端开发工程师",
-			jonTask:"前端",
-			jobContent:"1. 负责网站的前端开发，配合UI依据客户的设计需求，完成网站的静态网页设计，并实现页面动态交互效果；2. 负责网站页面优化设计；3. 支持产品维护和新产品的开发，负责产品前端优化；4、根据网站开发工作流程，达到网站页面的时间和质量要求",
+			companyLogo:'images/icon/expre_logo1.png',
+			companyNam:"亚信科技",
+			companyWebsite:"http://www.asiainfo.com.cn/index.html;jsessionid=62355E2A4EE79A92B09256B3A58F7489",
+			jobPost:"前端开发",
+			jonTask:"前端开发工程师",
+			jobContent:"1、负责联通wo邮箱用户俱乐部的前端开发工作，配合UI依据客户的设计需求，完成网站的静态网页设计，并实现页面动态交互效果（普遍原生js，html，css，以及少量jq）;2、兼容IE、chrome、火狐、opera等浏览器并负责网站页面优化设计；满足甲方（产品经理）的功能需求，并随时和UI及产品经理沟通，规定时间内完成项目并上线；3、支持产品维护和新页面的开发，负责页面前端优化；4、根据网站开发工作流程，达到网站页面的时间和质量要求；5、根据需求制作活动页面，包括：签到累计积分、积分兑换礼包等。地址：http://club.mail.wo.cn/shop/index.shtml"
 		}
 ]
